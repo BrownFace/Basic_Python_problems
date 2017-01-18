fd888061365880da37b0f9844dfa274095da5fdd
Abdulrahman Alfawzan <mhagr500@gmail.com>
2017-01-18 13:13:19 -0600

    Add files via upload

---
+++ staircase.py
@@ -0,0 +1,15 @@
+# Stairs
+# Example below
+# #
+# ##
+# ###
+# ####
+# #####
+
+# n is an argument that takes any int
+def  StairCase(n):
+    i = 0
+# While loop used to make sure i is not greater than n
+    while (i<=n):
+        print("#"*i)
+        i+=1
